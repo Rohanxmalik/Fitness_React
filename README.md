3d62f5a2547405cbec027de7faa97bdfa5464d1c
Rohanxmalik <rohanxmalik06@gmail.com>
2022-09-05 19:09:14 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# Fitness_React" 
+this is a project made by rohan malik
+tech used html css javascript react 
+integrated apis from rapid api 
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '47f93bd805mshc57e5176bc9004dp13a167jsn3cb72a7f6a75',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '47f93bd805mshc57e5176bc9004dp13a167jsn3cb72a7f6a75',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
